1b1c77368863334ae1d53b86c4582f32d34e1caa
davidladeji <71533464+davidladeji@users.noreply.github.com>
2025-06-25 12:26:35 -0700

    Initial commit

---
+++ src/main/java/com/dladeji/earthquake/EarthquakeTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.dladeji.earthquake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthquakeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthquakeTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dladeji/earthquake_tracker/EarthquakeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dladeji.earthquake_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthquakeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
