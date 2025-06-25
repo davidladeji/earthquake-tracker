fdcb1be03374dafdbc05f082a5038f203a34b9ec
davidladeji <71533464+davidladeji@users.noreply.github.com>
2025-06-25 16:32:16 -0700

    Create earthquakes table

---
+++ src/main/resources/db/migration/V1__create_earthquake_table.sql
@@ -0,0 +1,11 @@
+CREATE Table earthquake (
+    id          BIGINT AUTO_INCREMENT NOT NULL,
+    mag         DOUBLE NOT NULL,
+    type        VARCHAR(255) NOT NULL,
+    time        DATETIME NOT NULL,
+    city        VARCHAR(255) NOT NULL,
+    country     VARCHAR(255) NOT NULL,
+    place       VARCHAR(255) NOT NULL,
+
+    CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+)
